4226653b9e2cd5b93a9f7728b3224dcc4b8d35d4
earthlydev <jarizala.dev@gmail.com>
2023-02-01 11:30:22 -0800

    connected data file to card component.

--- airbnb-clone/src/App.jsx
+++ airbnb-clone/src/App.jsx
@@ -3,29 +3,12 @@ import Hero from "./components/Hero"
 import Card from "./components/Card"
 import data from "../data.js"
 
-/*
-Challenge:
-
-- import the array of data from data.js
-- map over the array to create <Card /> components
-- display the array of card components under the navbar
-  (in place of the current <Card /> component)
-
-Note: We haven't styled the group of components yet, so they'll
-still be block elements, stacked vertically. We'll add styling later.
-*/
-
 function App() {
     const cards = data.map(item => {
         return (
             <Card 
                 key={item.id || index}
-                coverImg={item.coverImg}
-                rating={item.stats.rating}
-                reviewCount={item.stats.reviewCount}
-                location={item.location}
-                title={item.title}
-                price={item.price}
+                {...item}
             />
         )
     })
@@ -34,7 +17,9 @@ function App() {
         <div>
             <Navbar />
             <Hero /> 
-            {cards}
+            <section className="card--list flex">
+                {cards}
+            </section>
         </div>
     )
 }
--- airbnb-clone/src/components/Card.jsx
+++ airbnb-clone/src/components/Card.jsx
@@ -1,13 +1,21 @@
 import Star from "../assets/star.png"
 
 function Card(props) {
+    let badgeText;
+    if (props.openSpots === 0 ) {
+        badgeText = "SOLD OUT"
+    }else if (props.location === "Online"){
+        badgeText = "ONLINE"
+    }
+
     return (
-        <section className="card fs-300">
+        <section className="card fs-300 flex">
+            {badgeText && <div className="card--badge">{badgeText}</div>}
             <img src={`../src/assets/${props.coverImg}`} className="card--img"/>
             <div className="card--stats flex">
                     <img src={Star} alt="red star" className="card--star"/>
-                <span>{props.rating}</span>
-                <span className="text-grey">({props.reviewCount}) •</span>
+                <span>{props.stats.rating}</span>
+                <span className="text-grey">({props.stats.reviewCount}) •</span>
                 <span className="text-grey">{props.location}</span>
             </div>
             <p>{props.title}</p>
